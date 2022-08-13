0a1f9d219515dc84cd65cc903f23ab24b30570f0
OwlHerring <mark@mora-fretheim.com>
2022-08-13 15:49:54 +0900

    Add several functions, honestly too many
    I should have committed sooner. I added several real functions
    and several dummy functions with dummy functionality to be replaced
    by actual functions.

--- resources/script.js
+++ resources/script.js
@@ -4,9 +4,36 @@ function getComputerChoice() {
     const c = Math.floor(Math.random()*3);    
     return choices[c];
 }
+function getPlayerChoice() {
+    let myPrompt = "What will you play? Rock? Paper? or Scissors?";
+    let stillUndecided = true;
+    let c; let a; // c: choice; a: answer
 
-let myLog = "";
-for(let i = 0; i < 25; i++){
-    myLog += getComputerChoice() + " ";
+    while(stillUndecided){
+        a = prompt(myPrompt); 
+        // I'll put something here to break the flow if a is escape or something.
+        c = convertChoiceToArray(a); 
+        // the plan is to make it so convertChoiceToArray returns something falsy to c if the answer is wrong.
+        if(c){
+            stillUndecided = false;
+        }
+        else {
+            myPrompt = "Please enter 'rock', 'paper', or 'scissors'.";
+        }
+    }
+    
 }
-alert(myLog); 
+function convertChoiceToArray(answer){
+    // This should take the choice in script form and return the array object, case-insensitively.
+    // For now it only returns "rock".
+    return "rock";
+}
+
+function gameRound(playerChoice,computerChoice){
+    console.log("Start!");
+    let c = getPlayerChoice();
+    console.log(`You played ${choices[c]}.`);
+}
+
+gameRound();
+
