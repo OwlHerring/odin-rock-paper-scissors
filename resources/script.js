7d05e52627a9bbcd01d6c5bbb0b15fa36ba901f9
OwlHerring <mark@mora-fretheim.com>
2022-08-13 15:25:54 +0900

    Create global variable array choices
    The choices array will represent rock (0), paper (1) and scissors (2).

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,12 @@
+const choices = ["rock","paper","scissors"];
+
 function getComputerChoice() {
-    const choice = Math.floor(Math.random()*3);    
-    return choice;
+    const c = Math.floor(Math.random()*3);    
+    return choices[c];
 }
 
 let myLog = "";
 for(let i = 0; i < 25; i++){
-    myLog += getComputerChoice();
+    myLog += getComputerChoice() + " ";
 }
-alert(myLog);
+alert(myLog); 
