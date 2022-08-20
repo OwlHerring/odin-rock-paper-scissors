fadf4e4523106deceb9c615ed268103747a0eb69
OwlHerring <mark@mora-fretheim.com>
2022-08-20 21:34:18 +0900

    Change container to button-container for clarity
    I intend to put button-container within another div, so
    I changed its name to be more specific.

--- resources/script.js
+++ resources/script.js
@@ -4,7 +4,7 @@ const choices = ["rock","paper","scissors"];
 // here is where I'll put the DOM elements. 
 // It's after the choices array so I can generate buttons based on it.
 
-let divCont = document.body.querySelector('div');
+let divCont = document.body.querySelector('.button-container');
 let myButtons = [];
 for(let i = 0; i < choices.length; i++){
     myButtons[i] = document.createElement('button');
